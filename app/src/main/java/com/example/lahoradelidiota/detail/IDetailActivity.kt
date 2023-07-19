ec6060f6c21711458efa6047630b6edb61f646c1
FireKotlin <fire3456gamer@gmail.com>
2023-07-19 02:16:01 -0600

    Extended FAB added.

--- app/src/main/java/com/example/lahoradelidiota/detail/IDetailActivity.kt
+++ app/src/main/java/com/example/lahoradelidiota/detail/IDetailActivity.kt
@@ -8,7 +8,9 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.TextView
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,26 +19,32 @@ import com.example.lahoradelidiota.databinding.ActivityMainBinding
 import com.example.lahoradelidiota.others.Idiota
 import com.example.lahoradelidiota.databinding.DetailActivityBinding
 import com.example.lahoradelidiota.main.MainActivity
-import com.google.android.material.snackbar.Snackbar
 
 @Suppress("DEPRECATION")
 class IDetailActivity : AppCompatActivity() {
 
+    // Ocultar FAB
     private lateinit var handler: Handler
     private lateinit var hideFabRunnable: Runnable
     private lateinit var binding: DetailActivityBinding
     private var fabHidden = false
-    private var isMenuOpen = false
+
+    // Expandible
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim) }
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim) }
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim) }
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim) }
+
+    private var clicked = false
 
     companion object {
         const val IDIOT_KEY = "idiota"
-
     }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DetailActivityBinding.inflate(layoutInflater)
+        binding = DetailActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         handler = Handler()
@@ -61,32 +69,17 @@ class IDetailActivity : AppCompatActivity() {
 
                     // Reinicia el temporizador de ocultar el FAB
                     handler.removeCallbacks(hideFabRunnable)
-                    handler.postDelayed(hideFabRunnable, 1000) // Oculta el FAB después de 1 segundo
+                    handler.postDelayed(hideFabRunnable, 7000) // Oculta el FAB después de 1 segundo
                 }
             }
             false
         }
 
         // Inicia el temporizador de ocultar el FAB al iniciar la actividad
-        handler.postDelayed(hideFabRunnable, 1000) // Oculta el FAB después de 1 segundo
+        handler.postDelayed(hideFabRunnable, 7000) // Oculta el FAB después de 1 segundo
 
         // Mostrar el FAB inicialmente
         binding.extendedFab.visibility = View.VISIBLE
-
-        binding.extendedFab.setOnClickListener {
-            toggleMenu()
-        }
-
-        binding.buttonOption1.setOnClickListener {
-            // Acción del primer botón
-            showSnackbar("Primer botón presionado")
-        }
-
-        binding.buttonOption2.setOnClickListener {
-            // Acción del segundo botón
-            showSnackbar("Segundo botón presionado")
-        }
-
 
         val idiota = intent.extras?.getParcelable<Idiota>(IDIOT_KEY)!!
 
@@ -97,7 +90,6 @@ class IDetailActivity : AppCompatActivity() {
         binding.habilidadEspecial.text = idiota.habilidadEspecial
         binding.descripcion.text = idiota.descripcion
 
-
         val toolbar = binding.detailtoolbar
         toolbar.title = idiota.nombre
         toolbar.setNavigationIcon(R.drawable.baseline_arrow_back_24)
@@ -105,8 +97,17 @@ class IDetailActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
         }
 
-
+        binding.extendedFab.setOnClickListener {
+            onAddButtonClicked()
+        }
+        binding.extendedFab1.setOnClickListener {
+            Toast.makeText(this, "Download Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+        binding.extendedFab2.setOnClickListener {
+            Toast.makeText(this, "Share Button Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
+
     override fun onPause() {
         super.onPause()
         // Detiene el temporizador de ocultar el FAB al pausar la actividad
@@ -120,20 +121,49 @@ class IDetailActivity : AppCompatActivity() {
             .start()
     }
 
-    private fun toggleMenu() {
-        isMenuOpen = !isMenuOpen
-        if (isMenuOpen) {
-            binding.buttonOption1.visibility = View.VISIBLE
-            binding.buttonOption2.visibility = View.VISIBLE
+    private fun onAddButtonClicked() {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        clicked = !clicked
+    }
+
+    private fun setVisibility(clicked: Boolean) {
+        if (!clicked) {
+            binding.extendedFab1.visibility = View.VISIBLE
+            binding.extendedFab2.visibility = View.VISIBLE
         } else {
-            binding.buttonOption1.visibility = View.GONE
-            binding.buttonOption2.visibility = View.GONE
+            binding.extendedFab1.visibility = View.INVISIBLE
+            binding.extendedFab2.visibility = View.INVISIBLE
         }
     }
 
-    private fun showSnackbar(message: String) {
-        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
+    private fun setAnimation(clicked: Boolean) {
+        if (!clicked) {
+            binding.extendedFab1.apply {
+                visibility = View.VISIBLE
+                translationY = 0f
+            }
+
+            binding.extendedFab2.apply {
+                visibility = View.VISIBLE
+                translationY = 0f
+            }
+
+            binding.extendedFab.apply {
+                rotation = 0f
+            }
+        } else {
+            binding.extendedFab1.apply {
+                translationY = 300f
+            }
+
+            binding.extendedFab2.apply {
+                translationY = 600f
+            }
+
+            binding.extendedFab.apply {
+                rotation = 180f
+            }
+        }
     }
-
 }
-
